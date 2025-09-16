db5b8d6a67b52d002b3a34ac2b86bce7489797c7
macrobian88 <ramshankarsc9@gmail.com>
2025-09-16 17:12:48 +0530

    Add desktop configuration at root level

---
+++ wix_integration/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Wix Integration",
+            "color": "grey",
+            "icon": "octicon octicon-file-directory",
+            "type": "module",
+            "label": _("Wix Integration")
+        }
+    ]
